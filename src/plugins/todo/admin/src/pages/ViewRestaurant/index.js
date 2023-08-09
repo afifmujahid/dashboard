d04048e11681b04a4db9d8b753b4f8a58965a2e9
Fatin <fatinnajwamaziyyah@i-csi.org>
2023-08-09 11:31:55 +0800

    view restaurant step

--- src/plugins/todo/admin/src/pages/ViewRestaurant/index.js
+++ src/plugins/todo/admin/src/pages/ViewRestaurant/index.js
@@ -13,7 +13,30 @@ import EmployeeNumber from "../../components/EmployeeNumber";
 
 const ViewRestaurant = ({ location }) => {
   const restaurantId = location.state.todo.id;
+  const [selectedStep, setSelectedStep] = useState(1);
 
+  const handleCardClick = (step) => {
+    setSelectedStep(step);
+  };
+  const steps = [1, 2, 3, 4, 5, 6, 7];
+  const getTitle = (step) => {
+    switch (step) {
+      case 1:
+        return "Restaurant Detail";
+      case 2:
+        return "Number of Employee";
+      case 3:
+        return "Key Personnel Responsible";
+      case 4:
+        return "Operating Hour";
+      case 5:
+        return "Image Attachment";
+      case 6:
+        return "Document Submission";
+      case 7:
+        return "Cleanliness";
+    }
+  };
   return (
     <Box background="neutral100">
       <Layout>
@@ -23,28 +46,61 @@ const ViewRestaurant = ({ location }) => {
           hasRadius={true}
           shadow="filterShadow"
           padding={8}
-          style={{ marginTop: "10px" }}
+          style={{
+            marginTop: "10px",
+            display: "grid",
+            gridTemplateColumns: "300px 1fr",
+          }}
         >
-          <div style={{ display: "grid", gridTemplateColumns: "200px 1fr" }}>
-            <Card
-              style={{
-                display: "grid",
-                rowGap: "16px",
-                width: "48px",
-                height: "48px",
-                borderRadius: "10px",
-                backgroundColor: "#004e5a",
-                color: "white",
-                alignItems: "center",
-                justifyContent: "center",
-              }}
-            >
-              1
-            </Card>
-            <ContentLayout>
-              <EmployeeNumber restaurantId={restaurantId}></EmployeeNumber>
-            </ContentLayout>
+          <div style={{ display: "grid", rowGap: "16px" }}>
+            {steps.map((step) => (
+              <div
+                key={step}
+                style={{
+                  display: "grid",
+                  gridTemplateColumns: "auto 1fr",
+                  columnGap: "8px",
+                  alignItems: "center",
+                }}
+              >
+                <Card
+                  style={{
+                    display: "grid",
+                    width: "48px",
+                    height: "48px",
+                    borderRadius: "10px",
+                    backgroundColor:
+                      selectedStep === step ? "#004e5a" : "transparent",
+                    color: selectedStep === step ? "white" : "black",
+                    alignItems: "center",
+                    justifyContent: "center",
+                  }}
+                  onClick={() => handleCardClick(step)}
+                >
+                  {step}
+                </Card>
+                <Typography variant="body" fontWeight="semiBold">
+                  {getTitle(step)}
+                </Typography>
+              </div>
+            ))}
           </div>
+          <ContentLayout>
+            {selectedStep === 1 && (
+              <>
+                <h2 style={{ marginBottom: "28px", fontWeight: "semiBold" }}>
+                  Restaurant Detail
+                </h2>
+                <RestaurantDetail restaurantId={restaurantId} />
+              </>
+            )}
+            {selectedStep === 2 && (
+              <>
+                <h2 style={{ marginBottom: "28px" }}>Employee Number</h2>
+                <EmployeeNumber restaurantId={restaurantId} />
+              </>
+            )}
+          </ContentLayout>
         </Box>
       </Layout>
     </Box>
