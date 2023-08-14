68bb700a498d0be7ce242a95f752cb6c61cdbf2c
Afif <afifmujahid@i-csi.org>
2023-08-14 10:56:07 +0800

    export

--- src/plugins/todo/admin/src/components/RestaurantList/index.js
+++ src/plugins/todo/admin/src/components/RestaurantList/index.js
@@ -2,14 +2,17 @@ import React, { useState, useEffect } from "react";
 import { useHistory } from "react-router-dom";
 import todoRequests from "../../api/todo";
 import pluginId from "../../pluginId";
+import { Button } from "@strapi/design-system";
 import { Table, Thead, Tbody, Tr, Td, Th } from "@strapi/design-system/Table";
 import { Typography } from "@strapi/design-system/Typography";
 import { IconButton } from "@strapi/design-system/IconButton";
 import Eye from "@strapi/icons/Eye";
+import * as XLSX from "xlsx";
 
 const RestaurantList = () => {
   const history = useHistory();
   const [todoData, setTodoData] = useState([]);
+
   useEffect(() => {
     async function fetchTodos() {
       try {
@@ -22,66 +25,86 @@ const RestaurantList = () => {
     }
     fetchTodos();
   }, []);
+
+  const exportToExcel = () => {
+    const formattedData = todoData.map((todo) => ({
+      "No.": todo.id,
+      "Restaurant Name": todo.name,
+      "Officer In Charge Name": todo.officerInCharge,
+      "Created Date": todo.createdAt,
+    }));
+
+    const worksheet = XLSX.utils.json_to_sheet(formattedData);
+    const workbook = XLSX.utils.book_new();
+    XLSX.utils.book_append_sheet(workbook, worksheet, "Todo Data");
+    XLSX.writeFile(workbook, "todo_data.xlsx");
+  };
+
   return (
-    <Table>
-      <Thead>
-        <Tr>
-          <Th>
-            <Typography>No.</Typography>
-          </Th>
+    <div>
+      <Button onClick={exportToExcel}>Export to Excel</Button>
+      <Table>
+        <Thead>
+          <Tr>
+            <Th>
+              <Typography>No.</Typography>
+            </Th>
 
-          <Th>
-            <Typography>Name</Typography>
-          </Th>
+            <Th>
+              <Typography>Name</Typography>
+            </Th>
 
-          <Th>
-            <Typography>Officer In Charge Name</Typography>
-          </Th>
-          <Th>
-            <Typography>Created Date</Typography>
-          </Th>
-          <Th>
-            <Typography>Action</Typography>
-          </Th>
-        </Tr>
-      </Thead>
-      <Tbody>
-        {todoData.map((todo, index) => {
-          const count = index + 1;
-          return (
-            <Tr key={todo.id}>
-              <Td>
-                <Typography textColor="neutral800">{count}</Typography>
-              </Td>
-              <Td>
-                <Typography textColor="neutral800">{todo.name}</Typography>
-              </Td>
-              <Td>
-                <Typography textColor="neutral800">
-                  {todo.officerInCharge}
-                </Typography>
-              </Td>
-              <Td>
-                <Typography textColor="neutral800">{todo.createdAt}</Typography>
-              </Td>
-              <Td>
-                <IconButton
-                  label="View"
-                  noBorder
-                  icon={<Eye />}
-                  onClick={() =>
-                    history.push({
-                      pathname: `/plugins/${pluginId}/${todo.id}`,
-                      state: { todo },
-                    })
-                  }
-                />
-              </Td>
-            </Tr>
-          );
-        })}
-      </Tbody>
-    </Table>
+            <Th>
+              <Typography>Officer In Charge Name</Typography>
+            </Th>
+            <Th>
+              <Typography>Created Date</Typography>
+            </Th>
+            <Th>
+              <Typography>Action</Typography>
+            </Th>
+          </Tr>
+        </Thead>
+        <Tbody>
+          {todoData.map((todo, index) => {
+            const count = index + 1;
+            return (
+              <Tr key={todo.id}>
+                <Td>
+                  <Typography textColor="neutral800">{count}</Typography>
+                </Td>
+                <Td>
+                  <Typography textColor="neutral800">{todo.name}</Typography>
+                </Td>
+                <Td>
+                  <Typography textColor="neutral800">
+                    {todo.officerInCharge}
+                  </Typography>
+                </Td>
+                <Td>
+                  <Typography textColor="neutral800">
+                    {todo.createdAt}
+                  </Typography>
+                </Td>
+                <Td>
+                  <IconButton
+                    label="View"
+                    noBorder
+                    icon={<Eye />}
+                    onClick={() =>
+                      history.push({
+                        pathname: `/plugins/${pluginId}/${todo.id}`,
+                        state: { todo },
+                      })
+                    }
+                  />
+                </Td>
+              </Tr>
+            );
+          })}
+        </Tbody>
+      </Table>
+    </div>
   );
 };
 export default RestaurantList;
