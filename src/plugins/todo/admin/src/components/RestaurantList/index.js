3afeb708a1d6cfdb1cf152fa2d011961df379386
Afif <afifmujahid@i-csi.org>
2023-08-15 11:07:40 +0800

    useQuery

--- src/plugins/todo/admin/src/components/RestaurantList/index.js
+++ src/plugins/todo/admin/src/components/RestaurantList/index.js
@@ -1,5 +1,6 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import { useHistory } from "react-router-dom";
+import { useQuery } from "react-query";
 import todoRequests from "../../api/todo";
 import pluginId from "../../pluginId";
 import { Button } from "@strapi/design-system";
@@ -11,50 +12,46 @@ import * as XLSX from "xlsx";
 
 const RestaurantList = () => {
   const history = useHistory();
-  const [todoData, setTodoData] = useState([]);
 
-  useEffect(() => {
-    async function fetchTodos() {
-      try {
-        const todos = await todoRequests.getAllTodos();
-        const sortedTodos = todos.sort((a, b) => a.id - b.id);
-        setTodoData(sortedTodos);
-      } catch (error) {
-        console.error("Error fetching todos:", error);
-      }
-    }
-    fetchTodos();
-  }, []);
+  const fetchTodos = async () => {
+    const todos = await todoRequests.getAllTodos();
+    const sortedTodos = todos.sort((a, b) => a.id - b.id);
+    return sortedTodos;
+  };
+
+  const { data: todoData, error } = useQuery("todos", fetchTodos);
 
   const exportToExcel = () => {
-    const formattedData = todoData.map((todo, index) => ({
-      "No.": index + 1,
-      "Restaurant Name": todo.name,
-      "Officer In Charge Name": todo.officerInCharge,
-      "Created Date": new Date(todo.createdAt).toLocaleDateString("en-GB"),
-      "Registeration Name": todo.registrationName,
-      "Registeration No": todo.registrationNo,
-      "Company Status": todo.status,
-      "Company Address": todo.address,
-      District: todo.district,
-      Postcode: todo.postcode,
-      City: todo.city,
-      State: todo.state,
-      Latitude: todo.latitude,
-      Longtitude: todo.longitude,
-      "Phone Number": todo.phone,
-      Email: todo.email,
-      "Number of Shifts": todo.numberOfShift,
-      "Annual Sales Revenue RM": todo.revenue,
-      "Business Type": todo.businessType,
-      "Type of Industry": todo.industryType,
-      "Product Market": todo.productMarket,
-    }));
+    if (todoData) {
+      const formattedData = todoData.map((todo, index) => ({
+        "No.": index + 1,
+        "Restaurant Name": todo.name,
+        "Officer In Charge Name": todo.officerInCharge,
+        "Created Date": new Date(todo.createdAt).toLocaleDateString("en-GB"),
+        "Registeration Name": todo.registrationName,
+        "Registeration No": todo.registrationNo,
+        "Company Status": todo.status,
+        "Company Address": todo.address,
+        District: todo.district,
+        Postcode: todo.postcode,
+        City: todo.city,
+        State: todo.state,
+        Latitude: todo.latitude,
+        Longtitude: todo.longitude,
+        "Phone Number": todo.phone,
+        Email: todo.email,
+        "Number of Shifts": todo.numberOfShift,
+        "Annual Sales Revenue RM": todo.revenue,
+        "Business Type": todo.businessType,
+        "Type of Industry": todo.industryType,
+        "Product Market": todo.productMarket,
+      }));
 
-    const worksheet = XLSX.utils.json_to_sheet(formattedData);
-    const workbook = XLSX.utils.book_new();
-    XLSX.utils.book_append_sheet(workbook, worksheet, "Restaurant List");
-    XLSX.writeFile(workbook, "Restaurant List.xlsx");
+      const worksheet = XLSX.utils.json_to_sheet(formattedData);
+      const workbook = XLSX.utils.book_new();
+      XLSX.utils.book_append_sheet(workbook, worksheet, "Restaurant List");
+      XLSX.writeFile(workbook, "Restaurant List.xlsx");
+    }
   };
 
   return (
@@ -94,42 +91,43 @@ const RestaurantList = () => {
           </Tr>
         </Thead>
         <Tbody>
-          {todoData.map((todo, index) => {
-            const count = index + 1;
-            return (
-              <Tr key={todo.id}>
-                <Td>
-                  <Typography textColor="neutral800">{count}</Typography>
-                </Td>
-                <Td>
-                  <Typography textColor="neutral800">{todo.name}</Typography>
-                </Td>
-                <Td>
-                  <Typography textColor="neutral800">
-                    {todo.officerInCharge}
-                  </Typography>
-                </Td>
-                <Td>
-                  <Typography textColor="neutral800">
-                    {new Date(todo.createdAt).toLocaleDateString("en-GB")}
-                  </Typography>
-                </Td>
-                <Td>
-                  <IconButton
-                    label="View"
-                    noBorder
-                    icon={<Eye />}
-                    onClick={() =>
-                      history.push({
-                        pathname: `/plugins/${pluginId}/${todo.id}`,
-                        state: { todo },
-                      })
-                    }
-                  />
-                </Td>
-              </Tr>
-            );
-          })}
+          {todoData &&
+            todoData.map((todo, index) => {
+              const count = index + 1;
+              return (
+                <Tr key={todo.id}>
+                  <Td>
+                    <Typography textColor="neutral800">{count}</Typography>
+                  </Td>
+                  <Td>
+                    <Typography textColor="neutral800">{todo.name}</Typography>
+                  </Td>
+                  <Td>
+                    <Typography textColor="neutral800">
+                      {todo.officerInCharge}
+                    </Typography>
+                  </Td>
+                  <Td>
+                    <Typography textColor="neutral800">
+                      {new Date(todo.createdAt).toLocaleDateString("en-GB")}
+                    </Typography>
+                  </Td>
+                  <Td>
+                    <IconButton
+                      label="View"
+                      noBorder
+                      icon={<Eye />}
+                      onClick={() =>
+                        history.push({
+                          pathname: `/plugins/${pluginId}/${todo.id}`,
+                          state: { todo },
+                        })
+                      }
+                    />
+                  </Td>
+                </Tr>
+              );
+            })}
         </Tbody>
       </Table>
     </div>
