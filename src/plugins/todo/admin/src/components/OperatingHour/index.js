be560bf9c2fbda13d5ae1f52878c3bb2effab37b
Fatin <fatinnajwamaziyyah@i-csi.org>
2023-08-15 11:34:29 +0800

    operating hour border

--- src/plugins/todo/admin/src/components/OperatingHour/index.js
+++ src/plugins/todo/admin/src/components/OperatingHour/index.js
@@ -1,6 +1,14 @@
 import React, { useState, useEffect } from "react";
 import todoRequests from "../../api/todo";
-import { Typography } from "@strapi/design-system";
+import {
+  Typography,
+  Table,
+  Thead,
+  Tbody,
+  Tr,
+  Td,
+  Th,
+} from "@strapi/design-system";
 
 const OperatingHour = ({ restaurantId }) => {
   const [todoData, setTodoData] = useState(null);
@@ -18,39 +26,68 @@ const OperatingHour = ({ restaurantId }) => {
     fetchOperatingHour();
   }, [selectedRestaurant]);
 
+  const cellStyle = {
+    borderBottom: "1px solid black",
+    borderRight: "1px solid black",
+    padding: "8px",
+  };
+
+  const headerCellStyle = {
+    ...cellStyle,
+  };
+
   return (
     <div
       style={{
         display: "grid",
         gridTemplateColumns: "repeat(3, 1fr)",
-        rowGap: "16px",
-        gap: "16px",
+        borderLeft: "1px solid black",
+        borderTop: "1px solid black",
       }}
     >
-      <div />
-      <Typography fontWeight="semiBold">From</Typography>
-      <Typography fontWeight="semiBold">To</Typography>
-      <Typography fontWeight="semiBold">Sunday</Typography>
-      <Typography>{todoData?.name}</Typography>
-      <Typography>{todoData?.registrationName}</Typography>
-      <Typography fontWeight="semiBold">Monday</Typography>
-      <Typography>{todoData?.status}</Typography>
-      <Typography>{todoData?.district}</Typography>
-      <Typography fontWeight="semiBold">Tuesday</Typography>
-      <Typography>{todoData?.latitude}</Typography>
-      <Typography>{todoData?.longitude}</Typography>
-      <Typography fontWeight="semiBold">Wednesday</Typography>
-      <Typography></Typography>
-      <Typography></Typography>
-      <Typography fontWeight="semiBold">Thursday</Typography>
-      <Typography></Typography>
-      <Typography></Typography>
-      <Typography fontWeight="semiBold">Friday</Typography>
-      <Typography></Typography>
-      <Typography></Typography>
-      <Typography fontWeight="semiBold">Saturday</Typography>
-      <Typography></Typography>
-      <Typography></Typography>
+      <div style={headerCellStyle}></div>
+
+      <Typography style={headerCellStyle} fontWeight="semiBold">
+        From
+      </Typography>
+      <Typography style={headerCellStyle} fontWeight="semiBold">
+        To
+      </Typography>
+      <Typography style={cellStyle} fontWeight="semiBold">
+        Sunday
+      </Typography>
+      <Typography style={cellStyle}>{todoData?.name}</Typography>
+      <Typography style={cellStyle}>{todoData?.registrationName}</Typography>
+      <Typography style={cellStyle} fontWeight="semiBold">
+        Monday
+      </Typography>
+      <Typography style={cellStyle}>{todoData?.status}</Typography>
+      <Typography style={cellStyle}>{todoData?.district}</Typography>
+      <Typography style={cellStyle} fontWeight="semiBold">
+        Tuesday
+      </Typography>
+      <Typography style={cellStyle}>{todoData?.latitude}</Typography>
+      <Typography style={cellStyle}>{todoData?.longitude}</Typography>
+      <Typography style={cellStyle} fontWeight="semiBold">
+        Wednesday
+      </Typography>
+      <Typography style={cellStyle}></Typography>
+      <Typography style={cellStyle}></Typography>
+      <Typography style={cellStyle} fontWeight="semiBold">
+        Thursday
+      </Typography>
+      <Typography style={cellStyle}></Typography>
+      <Typography style={cellStyle}></Typography>
+      <Typography style={cellStyle} fontWeight="semiBold">
+        Friday
+      </Typography>
+      <Typography style={cellStyle}></Typography>
+      <Typography style={cellStyle}></Typography>
+      <Typography style={cellStyle} fontWeight="semiBold">
+        Saturday
+      </Typography>
+      <Typography style={cellStyle}></Typography>
+      <Typography style={cellStyle}></Typography>
     </div>
   );
 };
